76cb639dbe4d2f771ea5b5206b394d945d5a4457
Luis José Sánchez <luisjoseprofe@gmail.com>
2016-05-14 12:37:45 +0200

    form updated with all validation

--- js/app.js
+++ js/app.js
@@ -1,73 +1,88 @@
-    var myApp = angular.module('confusionApp', []);
-    myApp.controller('MenuController', function() {
-      
-      this.tab = 1;
-      
-      this.filtText = '';
-      
-      this.filters = ['', '', 'appetizer', 'main', 'dessert'];
-      
-      this.dishes = [
-        {
-          image: 'images/uthapizza.png',
-          category: 'main',
-          name: 'Uthapizza',
-          label: 'Hot',
-          price: 4.99,
-          description: 'A unique combination of Uthappam (pancake) and italian pizza, topped with Cerignola olivesLorem fistrum va usté muy cargadoo no te digo trigo por no llamarte Rodrigor fistro condemor mamaar la caidita está la cosa muy malar te va a hasé pupitaa.'
-        },
-        {
-          image: 'images/zucchipakoda.png',
-          name: 'Zucchipakoda',
-          category: 'appetizer',
-          price: 1.99,
-          description: 'Me cago en tus muelas ahorarr qué dise usteer de la pradera de la pradera va usté muy cargadoo tiene musho peligro te voy a borrar el cerito. Fistro diodeno diodeno no te digo trigo por no llamarte Rodrigor torpedo.'
-        },
-        {
-          image: 'images/vadonut.png',
-          category: 'dessert',
-          name: 'Vadonut',
-          label: 'New',
-          price: 1.99,
-          description: 'A unique combination of Uthappam (pancake) and italian pizza, topped with Cerignola olivesLorem fistrum va usté muy cargadoo no te digo trigo por no llamarte Rodrigor fistro condemor mamaar la caidita está la cosa muy malar te va a hasé pupitaa.'
-        },
-        {
-          image: 'images/elaicheesecake.png',
-          category: 'dessert',
-          name: 'Elai Cheese Cake',
-          price: 2.99,
-          description: '   Fistro tiene musho peligro benemeritaar llevame al sircoo está la cosa muy malar se calle ustée a wan no te digo trigo por no llamarte Rodrigor qué dise usteer. Me cago en tus muelas al ataquerl no puedor sexuarl te va a hasé pupitaa quietooor. Fistro me cago en tus muelas caballo blanco caballo negroorl ese pedazo de a gramenawer condemor sexuarl.'
-        },
-      ];
-      
-      this.select = function(setTab) {
-        this.tab = setTab;
-        this.filtText = this.filters[setTab];
-      }
-      
-      this.isSelected = function(checkTab) {
-        return (this.tab === checkTab);
-      }
-    });
+var myApp = angular.module('confusionApp', []);
+myApp.controller('MenuController', function () {
 
-    myApp.controller('ContactController', ['$scope', function($scope) {
-      $scope.feedback = {
-        myChannel: "",
-        firstName: "",
-        lastName: "",
-        agree: "",
-        email: ""
-      };
-      
-      $scope.channels = [
-        {value: "tel", label: "Tel."},
-        {value: "Email", label: "Email"}
-      ];
-      
+  this.tab = 1;
+
+  this.filtText = '';
+
+  this.filters = ['', '', 'appetizer', 'main', 'dessert'];
+
+  this.dishes = [
+    {
+      image: 'images/uthapizza.png',
+      category: 'main',
+      name: 'Uthapizza',
+      label: 'Hot',
+      price: 4.99,
+      description: 'A unique combination of Uthappam (pancake) and italian pizza, topped with Cerignola olivesLorem fistrum va usté muy cargadoo no te digo trigo por no llamarte Rodrigor fistro condemor mamaar la caidita está la cosa muy malar te va a hasé pupitaa.'
+    },
+    {
+      image: 'images/zucchipakoda.png',
+      name: 'Zucchipakoda',
+      category: 'appetizer',
+      price: 1.99,
+      description: 'Me cago en tus muelas ahorarr qué dise usteer de la pradera de la pradera va usté muy cargadoo tiene musho peligro te voy a borrar el cerito. Fistro diodeno diodeno no te digo trigo por no llamarte Rodrigor torpedo.'
+    },
+    {
+      image: 'images/vadonut.png',
+      category: 'dessert',
+      name: 'Vadonut',
+      label: 'New',
+      price: 1.99,
+      description: 'A unique combination of Uthappam (pancake) and italian pizza, topped with Cerignola olivesLorem fistrum va usté muy cargadoo no te digo trigo por no llamarte Rodrigor fistro condemor mamaar la caidita está la cosa muy malar te va a hasé pupitaa.'
+    },
+    {
+      image: 'images/elaicheesecake.png',
+      category: 'dessert',
+      name: 'Elai Cheese Cake',
+      price: 2.99,
+      description: '   Fistro tiene musho peligro benemeritaar llevame al sircoo está la cosa muy malar se calle ustée a wan no te digo trigo por no llamarte Rodrigor qué dise usteer. Me cago en tus muelas al ataquerl no puedor sexuarl te va a hasé pupitaa quietooor. Fistro me cago en tus muelas caballo blanco caballo negroorl ese pedazo de a gramenawer condemor sexuarl.'
+    }
+  ];
+
+  this.select = function (setTab) {
+    this.tab = setTab;
+    this.filtText = this.filters[setTab];
+  };
+
+  this.isSelected = function (checkTab) {
+    return (this.tab === checkTab);
+  };
+});
+
+myApp.controller('ContactController', ['$scope', function ($scope) {
+  $scope.feedback = {
+    myChannel: "",
+    firstName: "",
+    lastName: "",
+    agree: "",
+    email: ""
+  };
+
+  $scope.channels = [
+    {value: "tel", label: "Tel."},
+    {value: "Email", label: "Email"}
+  ];
+
+  $scope.invalidChannelSelection = false;
+
+}]);
+
+myApp.controller('FeedbackController', ['$scope', function ($scope) {
+
+  $scope.sendFeedback = function () {
+    console.log($scope.feedback);
+    if ($scope.feedback.agree && ($scope.feedback.mychannel == "")&& !$scope.feedback.mychannel) {
+      $scope.invalidChannelSelection = true;
+      console.log('incorrect');
+    } else {
       $scope.invalidChannelSelection = false;
-      
-    }]);
+      $scope.feedback = {mychannel:"", firstName:"", lastName:"",
+                         agree:false, email:"" };
+      $scope.feedback.mychannel="";
 
-    myApp.controller('FeedbackController', ['$scope', function($scope) {
-      
-    }]);
+      $scope.feedbackForm.$setPristine();
+      console.log($scope.feedback);
+    }
+  };
+}]);
